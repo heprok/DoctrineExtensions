5eb41bc2716fe34ed46f9700f011e2c93088d215
Oleg Kravchuck <ok@techno-les.com>
2021-02-16 18:52:07 +0800

    update

---
+++ src/Query/Postgresql/NowTime.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace DoctrineExtensions\Query\Postgresql;
+
+use Doctrine\ORM\Query\AST\Functions\FunctionNode;
+use Doctrine\ORM\Query\Lexer;
+use Doctrine\ORM\Query\Parser;
+use Doctrine\ORM\Query\SqlWalker;
+
+class NowTime extends FunctionNode
+{
+    private $expr1;
+
+    public function parse(Parser $parser)
+    {
+        $parser->match(Lexer::T_IDENTIFIER);
+        $parser->match(Lexer::T_OPEN_PARENTHESIS);
+        $parser->match(Lexer::T_CLOSE_PARENTHESIS);
+    }
+
+    public function getSql(SqlWalker $sqlWalker)
+    {
+        return sprintf(
+            'NOW()',
+        );
+    }
+}
